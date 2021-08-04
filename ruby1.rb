4ee3eac617165f6ca73c56e382820db6f644290c
mickee59 <hardworkhannas@gmail.com>
2021-08-04 17:45:15 +1000

    ruby file added
    This file was just uploaded

---
+++ ruby1.rb
@@ -0,0 +1,12 @@
+your_age =  21
+
+if your_age <= 17
+  puts "You are not old enough for this site"
+elsif your_age > 100
+  puts "http:\\snakeoil.com"
+elsif your_age == 21
+  puts "you just upgraded"
+else
+  puts " what is your gender"
+    #code
+end
